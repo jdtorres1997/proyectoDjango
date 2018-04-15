71c450bc07d6f4f75e3f3952212417b3b9a55a74
Juan David Torres <juan.torres.canon@correounivalle.edu.co>
2018-04-15 00:38:05 -0500

    Iniciando

---
+++ appprincipal/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+	path('login', views.autenticar, name='Autentication'),
+	path('', views.inicio, name='inicio'),
+	path('logout', auth_views.logout, {'next_page': '/'}, name='logout')
+]
---
+++ appprincipal/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+def autenticar(request):
+	
+	if(request.method == 'POST'):
+		action = request.POST.get('action', None)
+		username = request.POST.get('username', None)
+		password = request.POST.get('password', None)
+
+		if(action == 'signup'):
+			user = User.objects.create_user(username=username, password=password)
+			user.save()
+			
+		elif(action == 'login'):
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			
+		return redirect('/')
+	return render(request, 'login.html', {})
+	
+def inicio(request):
+	return render(request, 'inicio.html', {})
