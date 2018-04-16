a5a79606510511a4b68ca28199a90cd53f2deb31
jdtorres1997 <juan.torres.canon@correounivalle.edu.co>
2018-04-16 17:57:04 -0500

    Crud usuarios

--- appprincipal/urls.py
+++ appprincipal/urls.py
@@ -5,7 +5,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 	path('login', views.autenticar, name='Autentication'),
 	path('', views.inicio, name='inicio'),
-	path('logout', auth_views.logout, {'next_page': '/'}, name='logout'),
-	path('agregarusuario', views.agregarusuario, name='agregarusuario'),
+	path('logout', auth_views.logout, {'next_page': '/'}, name='logout'),	
 	path('usuarios', views.gestionarusuarios, name= 'gestionarusuarios'),
+	path('usuarios/new', views.agregarusuario, name='agregarusuario'),
+	path('usuarios/', views.gestionarusuarios, name= 'gestionarusuarios'),
+	path('usuarios/<pk>', views.detalleUsuario, name='crudUsuario'),
 ]
--- appprincipal/views.py
+++ appprincipal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.template import loader
@@ -25,24 +25,89 @@ def inicio(request):
 
 def agregarusuario(request):
 
-	if(request.method == 'POST'):
-		username = request.POST.get('username', None)
-		password = request.POST.get('password', None)
-		tipo = request.POST.get('tipo', None)
-		user = User.objects.create_user(username=username, password=password)
-		user.profile.tipo = tipo #Añadido
-		user.save() #Añadido
+	if(request.user.is_authenticated):
+		if(request.user.profile.tipo == 'admin'):
+			if(request.method == 'POST'):
+
+				username = request.POST.get('username', None)
+				name = request.POST.get('name', None)
+				password = request.POST.get('password', None)
+				email = request.POST.get('email', None)
+				tipo = request.POST.get('tipo', None)
+				user = User.delete
+				user = User.objects.create_user(username=username, password=password, email=email, last_name=name)
+				user.profile.tipo = tipo #Añadido
+				user.save() #Añadido
+				return redirect('/')
+
+			return render(request, 'agregarUsuario.html', {})
+		else:
+			return redirect('/')
+	else:
 		return redirect('/')
 
-	return render(request, 'agregarUsuario.html', {})
-
-
 def gestionarusuarios(request):
-	usuarios = User.objects.order_by('id')
-	template = loader.get_template('usuarios.html')
+	if(request.user.is_authenticated):
+		if(request.user.profile.tipo == 'admin'):
+			usuarios = User.objects.order_by('id')
+			template = loader.get_template('usuarios.html')
+			
+			context = {
+				'usuarios' : usuarios
+			}
+			return HttpResponse(template.render(context, request))
+		else:
+			return redirect('/')
+	else:
+		return redirect('/')
 	
-	context = {
-		'usuarios' : usuarios
-	}
-	return HttpResponse(template.render(context, request))
-	
+def detalleUsuario(request, pk):
+	if(request.user.is_authenticated):
+		if(request.user.profile.tipo == 'admin'):
+			if(request.method == 'POST'):
+				accion = request.POST.get('accion', None)
+
+				if(accion == "consultar"):
+					username = request.POST.get('username', None)
+					name = request.POST.get('name', None)
+					password = request.POST.get('password', None)
+					email = request.POST.get('email', None)
+					tipo = request.POST.get('tipo', None)
+					usuario = get_object_or_404(User, pk=pk)
+					if(username != ""):
+						usuario.username = username
+					if(password != ""):
+						usuario.password = password
+					if(tipo != ""):
+						usuario.profile.tipo = tipo
+					if(name != ""):
+						usuario.last_name = name
+					if(email != ""):
+						usuario.email = email
+					usuario.save()
+					
+					usuario = get_object_or_404(User, pk=pk) #Saca un objeto que su pk sea igual a la ingresada
+					template = loader.get_template('usuario.html') 
+					context = {
+						'usuario': usuario
+					}
+					return HttpResponse(template.render(context, request))
+
+				elif(accion == "eliminar"):
+					password = request.POST.get('password', None)
+					user = User.objects.get(pk=pk)
+					user.delete()
+
+					return redirect('/usuarios')
+
+			else:
+				usuario = get_object_or_404(User, pk=pk) #Saca un objeto que su pk sea igual a la ingresada
+				template = loader.get_template('usuario.html') 
+				context = {
+					'usuario': usuario
+				}
+				return HttpResponse(template.render(context, request))
+		else:
+			return redirect('/')
+	else:
+		return redirect('/')
